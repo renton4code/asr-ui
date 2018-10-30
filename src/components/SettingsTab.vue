5437cd96ab59fedb53fd7b57946152ff379c6a6e
Владислав Зотов <vzotov420@gmail.com>
2018-10-31 00:22:10 +0300

    ui:settings: style

--- src/components/SettingsTab.vue
+++ src/components/SettingsTab.vue
@@ -4,45 +4,53 @@
     class="mdl-progress mdl-js-progress mdl-progress__indeterminate centered"
   ></div>
   <div v-else class="settings-layout">
-    <div class="mld-grid">
-      <div class="mdl-cell mdl-cell--2-col">
-        <span>Choose ASR from list:</span>
-      </div>
-      <div class="mdl-cell mdl-cell--4-col">
-        <select @change="asrSelected($event)">
-          <option v-for="asr in asrOptions" :value="asr" >{{ asr }}</option>
-        </select>
-      </div>
-    </div>
-    <div class="mld-grid">
-      <div class="mdl-cell mdl-cell--2-col">
-        <span>Settings for {{currentAsr}}:</span>
-      </div>
-    </div>
-    <div class="mld-grid">
-      <div v-for="setting in asrSettings">
-        <div class="mdl-cell mdl-cell--4-col">
-          <span>{{ setting.title }}: </span>
-          <select v-if="setting.type === 'select'" v-model="setting.defaultValue">
-            <option v-for="opt in setting.options" :value="opt" >{{ opt }}</option>
-          </select>
-          <input v-else :type="setting.type" v-model="setting.defaultValue" />
-        </div>
-      </div>
-    </div>
-    <div class="mld-grid">
-      <div class="mdl-cell mdl-cell--2-col">
-        <span>Choose sound from bank:</span>
-      </div>
-      <div class="mdl-cell mdl-cell--4-col">
-        <select @change="soundSelected($event)">
-          <option v-for="sound in soundList" :value="sound.id" >{{ sound.title }}</option>
-        </select>
-      </div>
-    </div>
-    <div class="mld-grid">
-      <div class="mdl-cell mdl-cell--2-col">
-        <button @click="runPipeWithConfig">run</button>
+    <div>
+      <h4>Create a pipeline</h4>
+      <table class="mdl-data-table mdl-shadow--2dp">
+        <tbody>
+          <tr>
+            <td class="mdl-data-table__cell--non-numeric">Choose ASR from list:</td>
+            <td class="mdl-data-table__cell--non-numeric">
+              <select @change="asrSelected($event)">
+                <option v-for="asr in asrOptions" :value="asr" >{{ asr }}</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td class="mdl-data-table__cell--non-numeric">Settings for {{currentAsr}}</td>
+            <td></td>
+          </tr>
+          <tr v-for="setting in asrSettings">
+            <td class="mdl-data-table__cell--non-numeric">{{ setting.title }}:</td>
+            <td class="mdl-data-table__cell--non-numeric">
+              <select v-if="setting.type === 'select'" v-model="setting.defaultValue">
+                <option v-for="opt in setting.options" :value="opt" >{{ opt }}</option>
+              </select>
+              <input
+                v-else
+                :type="setting.type"
+                :class="getStyleForInputByType(setting.type)"
+                v-model="setting.defaultValue"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td class="mdl-data-table__cell--non-numeric">Choose sound from bank:</td>
+            <td class="mdl-data-table__cell--non-numeric">
+              <select @change="soundSelected($event)">
+                <option v-for="sound in soundList" :value="sound.id" >{{ sound.title }}</option>
+              </select>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="mld-grid">
+        <button
+          class="run-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+          @click="runPipeWithConfig"
+        >
+          Run
+        </button>
       </div>
     </div>
   </div>
@@ -102,6 +110,12 @@ export default class SettingsTab extends Vue {
     // TODO: implement
   }
 
+  public getStyleForInputByType(type: string): string {
+    if (type === 'checkbox') { return 'mdl-checkbox__input'; }
+    if (type === 'text') { return 'mdl-textfield__input'; }
+    return 'mdl-textfield__input';
+  }
+
   private mounted() {
     this.getAvailableAsrList()
       .then(() => {
@@ -137,5 +151,10 @@ export default class SettingsTab extends Vue {
     left: 50%
     transform: translate(-50%, -50%)
   .settings-layout
+    display: flex
+    flex-direction: column
+    align-items: center
     padding: 50px
+  .run-button
+    margin-top: 20px
 </style>
